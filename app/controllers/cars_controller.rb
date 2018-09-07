2d07fb9bc17140d9af6314052efea11eae6b0eeb
Lillian N <ngo.lilli94@gmail.com>
2018-09-07 11:50:21 -0400

    Added routes for cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,41 @@
 class CarsController < ApplicationController
+
+    def index
+        @cars = Car.all
+    end
+
+    def create
+        Car.create(car_params)
+
+        redirect_to :controller => 'cars', :action => 'index' 
+    end
+
+    def new
+        @car = Car.new
+    end
+
+    def edit
+        @car = Car.find(params[:id])
+    end
+
+    def show
+        @car = Car.find(params[:id])
+    end
+
+    def update
+        @car = Car.find(params[:id])
+        @car.update(course_params)
+
+        redirect_to :controller => 'cars', :action => 'show' 
+    end
+
+    def destroy
+        @car = Car.find(params[:id])
+        @car.destroy
+    end
+
+    private
+    def car_params
+        params.require(:car).permit(:nickname,:description, :default)
+    end
 end
