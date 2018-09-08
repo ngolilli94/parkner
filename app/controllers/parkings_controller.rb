d8dcd8f2833cd73464a170cc51dce79939520c16
Lillian N <ngo.lilli94@gmail.com>
2018-09-08 11:30:39 -0400

    Added links to index and formatted show page

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,2 +1,44 @@
 class ParkingsController < ApplicationController
+
+    before_action :authenticate_user!
+    
+    def index
+        @user = current_user
+    end
+
+    def create
+        Parking.create(parking_params)
+
+        redirect_to :controller => 'parkings', :action => 'index' 
+    end
+
+    def new
+        @parking = Parking.new
+    end
+
+    def edit
+        @parking = Parking.find(params[:id])
+    end
+
+    def show
+        @parking = Parking.find(params[:id])
+        @parkedCar = @parking.car
+    end
+
+    def update
+        @car = Car.find(params[:id])
+        @car.update(car_params)
+
+        redirect_to :controller => 'cars', :action => 'show' 
+    end
+
+    def destroy
+        @parking = Parking.find(params[:id])
+        @parking.destroy
+    end
+
+    private
+    def parking_params
+        params.require(:parking).permit(:user_id,:car_id)
+    end
 end
