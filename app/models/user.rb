10709a6a754907225124294cf8a185bd62fa2d35
Lillian N <ngo.lilli94@gmail.com>
2018-09-09 22:46:45 -0400

    Fixed database structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :parkings, dependent: :destroy
-  has_many :cars, through: :parkings
+  has_many :cars, dependent: :destroy
 end
