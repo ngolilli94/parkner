f980a18c6a9668f5ae37c8160bbc3fce89a8eb88
Chungzilla <chung.jazmine@gmail.com>
2018-09-10 20:35:03 -0400

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-<<<<<<< HEAD
-=======
   has_many :parkings, dependent: :destroy
->>>>>>> 449b8dd1bd4d651b821ee9cbb3e06beb9ac494a4
   has_many :cars, dependent: :destroy
 end
